8fe8d7c356e1ec946f03478d5e8ba986a496cae8
Tomy Besson <tobesson@student.42lyon.fr>
2026-06-19 19:03:59 +0200

    created todo for next week

--- src/codexion.c
+++ src/codexion.c
@@ -6,12 +6,23 @@
 /*   By: tobesson <tobesson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 11:20:59 by tobesson          #+#    #+#             */
-/*   Updated: 2026/06/19 18:27:05 by tobesson         ###   ########.fr       */
+/*   Updated: 2026/06/19 19:03:30 by tobesson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc.h"
 
+/*
+ * current state of the project and remaing thing to fix before submission
+ * TODO: Feat: Add a help message for the user when arguments are invalid
+ * TODO: For the parsing, allow -0 for compatible argument/ allow +0 and +xxx positives numbers
+ * TODO: Check all malloc return values and handle errors (actually the code crash if NULL is returned)
+ * TODO: Check all pthread_create return values, protect all pthread_mutex_lock and pthread_mutex_unlock with error handling
+ * also protect pthread_cond_wait and pthread_cond_signal with error handling (+ pthread_cond_broadcast and pthread_cond_destroy, pthread_join)
+ * TODO: Small bug in output terminal: whn coder burn out, some other coder may still write a bit after the message "has burned out"
+ * make sure to stop all coders before printing the burnout message
+ * TODO: run tests with normiette, valgrind, helgrind, drd, valgrind breaker (in sgoinfre)
+*/
 
 
 /*
